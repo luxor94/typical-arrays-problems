765a342ad815e0a14c62f6bf9cab30e09586c685
luxor94 <47752490+luxor94@users.noreply.github.com>
2020-09-29 22:23:15 +0300

    feat: implemented the max, min, and avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( array == null) {
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+   array.sort(function(a, b) {
+    return a - b;
+  });
+  return array[0];
+  };
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array == null) {
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+  array.sort(function(a, b) {
+    return a - b;
+  });
+  return array[array.length-1];
+  };
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array == null) {
+    return 0;
+  } else if (array.length == 0) {
+    return 0;
+  } else {
+  let sum = 0;
+  for ( let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+
+  return sum / array.length;
+  };
 }
